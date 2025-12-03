7b2f73e72b081fa52647d1b9eb4f91d9c727afae
Mazen Saber <mazensaber1996@gmail.com>
2025-12-03 02:42:03 -0500

    Exponential Moving Averages Strategies

--- Main.cs
+++ Main.cs
@@ -1,6 +1,7 @@
 #region imports
 using QuantConnect.Brokerages;
 using QuantConnect.Data;
+using QuantConnect.Indicators;
 using QuantConnect.Orders;
 using System;
 #endregion
@@ -14,18 +15,20 @@ namespace QuantConnect.Algorithm.CSharp
         //UserVariables
         private string _ticker = "BTCUSD"; //virtual pai - tacks the current USD value of 1 BTC
         private string _baseSymbol = "BTC";
-        private int _startingCash = 1000;
-        private decimal _weight = 0.5m;
+        private int _startingCash = 2000;
         private CoinbaseBrokerageModel _coinBaseBrokerageModel = new CoinbaseBrokerageModel(AccountType.Cash);
-        private decimal _targetPrecent = 0.10m; // = +10% sell at that price increase
-        private decimal _stopPercent = 0.02m; // = -2% sell at that pricedecrease to minimize losses
-        private decimal _trailPercent = 0.03m;
+        private int maxPosition = 1000; // USD
+        private int minPosition = 100; // USD
+        private int fastPeriod = 720;
+        private int slowPeriod = 1560;
 
         //ProgramVariables
         public decimal _price;
         public decimal _holding; //number of BTCs that we hold in portfolio
-        public decimal _targetPrice;
-        public decimal _stopPrice;
+        public decimal usd;
+
+        public ExponentialMovingAverage _fastEMA;
+        public ExponentialMovingAverage _slowEMA;
 
 
         public override void Initialize()
@@ -36,8 +39,10 @@ namespace QuantConnect.Algorithm.CSharp
 
             AddCrypto(_ticker, Resolution.Minute);
 
-
             SetBrokerageModel(_coinBaseBrokerageModel);
+
+            _fastEMA = EMA(_ticker, fastPeriod, Resolution.Minute);
+            _slowEMA = EMA(_ticker, slowPeriod, Resolution.Minute);
         }
 
         /// OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
@@ -46,27 +51,31 @@ namespace QuantConnect.Algorithm.CSharp
         {
             _price = data[_ticker].Price;
 
+            usd = Portfolio.CashBook["USD"].Amount;
 
-
-            if (!Portfolio.Invested)
+            if (!Portfolio.Invested && usd > minPosition)
             {
-                SetHoldings(_ticker, _weight);
-                Log($"Purchased {_ticker} at {_price}");
-
-                _stopPrice = _price - (_price * _trailPercent);
-
+                if (_fastEMA > _slowEMA)
+                {
+                    decimal quantity = Math.Round(maxPosition / _price);
+                    MarketOrder(_ticker, quantity);
+                }
             }
-
-            _stopPrice = Math.Max(_price - (_price * _trailPercent), _stopPrice);
 
             if (Portfolio.Invested)
             {
                 _holding = Portfolio.CashBook[_baseSymbol].Amount;
-                if (_price < _stopPrice)
+
+                if (_fastEMA < _slowEMA)
                 {
                     Sell(_ticker, _holding);
                 }
             }
+
+            Plot("MA", "Fast", _fastEMA);
+            Plot("MA", "Slow", _slowEMA);
+            Plot("MA", "Price", _price);
+
         }
 
         public override void OnOrderEvent(OrderEvent orderEvent)
